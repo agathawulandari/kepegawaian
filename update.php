141e7dbd9b19e0e416f167eb50bc07024f4f61c2
Agatha Sari <agathawulandari02@gmail.com>
2026-04-30 23:00:28 +0700

    New

--- update.php
+++ update.php
@@ -4,12 +4,16 @@ include 'koneksi.php';
 $id = $_POST['id'];
 $nama = $_POST['nama'];
 $nip = $_POST['nip'];
-$jabatan = $_POST['jabatan'];
-$alamat = $_POST['alamat'];
-$no_hp = $_POST['no_hp'];
+$jk = $_POST['jk'];
+$pendidikan = $_POST['pendidikan'];
+$pangkat = $_POST['pangkat'];
+$jabatan_id = $_POST['jabatan_id'];
+$no_sk = $_POST['no_sk'];
+$tmt_sk = $_POST['tmt_sk'];
+$kelas_jabatan = $_POST['kelas_jabatan'];
 
-$stmt = mysqli_prepare($koneksi, "UPDATE pegawai SET nama=?, nip=?, jabatan=?, alamat=?, no_hp=? WHERE id=?");
-mysqli_stmt_bind_param($stmt, "sssssi", $nama, $nip, $jabatan, $alamat, $no_hp, $id);
+$stmt = mysqli_prepare($koneksi, "UPDATE pegawai SET nama=?, nip=?, jabatan_id=?, jk=?, pendidikan=?, pangkat=?, no_sk_terakhir=?, tmt_sk=?, kelas_jabatan=? WHERE id=?");
+mysqli_stmt_bind_param($stmt, "ssisssssii", $nama, $nip, $jabatan_id, $jk, $pendidikan, $pangkat, $no_sk, $tmt_sk, $kelas_jabatan, $id);
 mysqli_stmt_execute($stmt);
 
 header("Location: index.php?page=kepegawaian");
