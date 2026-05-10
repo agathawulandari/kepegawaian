961b80a163c42fa88e0e588262c43917f58585cb
Agatha Sari <agathawulandari02@gmail.com>
2026-05-10 23:52:18 +0700

    topbar

--- riwayat.php
+++ riwayat.php
@@ -148,45 +148,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 ?>
 
-<!-- Topbar -->
-<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-    <?php
-    $data = mysqli_fetch_assoc(mysqli_query($koneksi, "SELECT * FROM pegawai WHERE id = '$_SESSION[id]'"));
-    $username = $_SESSION['username'];
-    ?>
-    <!-- Topbar Navbar -->
-    <ul class="navbar-nav ml-auto">
-        <div class="topbar-divider d-none d-sm-block"></div>
-        <!-- Nav Item - User Information -->
-        <li class="nav-item dropdown no-arrow">
-            <a class="nav-link dropdown-toggle"
-                href="#"
-                id="userDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">
-                    <?= ucwords(strtolower($username)) ?>
-                </span>
-                <img class="img-profile rounded-circle"
-                    src="Logo imipas.png" />
-            </a>
-            <!-- Dropdown -->
-            <div class="dropdown-menu dropdown-menu-end shadow animated--grow-in"
-                aria-labelledby="userDropdown">
-                <a class="dropdown-item"
-                    href="#"
-                    data-bs-toggle="modal"
-                    data-bs-target="#logoutModal">
-                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Logout
-                </a>
-            </div>
-        </li>
-    </ul>
-</nav>
-<!-- End of Topbar -->
-
+<?php include 'template/topbar.php' ?>
 
 <!-- Begin Page Content -->
 <div class="container-fluid">
